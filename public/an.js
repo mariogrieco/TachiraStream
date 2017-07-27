ae36791478914b93e5c8457cf244c185237995b3
Mario Grieco <mariojosuexz@gmail.com>
2017-07-27 17:19:58 -0400

    anltc
    anltc

--- public/an.js
+++ public/an.js
@@ -3,5 +3,5 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-103519072-1', 'auto');
+ga('create', 'UA-103519072-1', 'http://tachira.herokuapp.com/');
 ga('send', 'pageview');
